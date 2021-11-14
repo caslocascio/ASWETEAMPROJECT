bffe3de28be1e411f0c1f73e4fd2ada07936c6a5
Lee Pierre <pierre@LeePierre.com>
2021-11-14 15:02:15 -0500

    add scraper and unittest

---
+++ utils/scraper.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+def getProfReview(Url, prof_name):
+    page = requests.get(Url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find("div", class_="card text-center")
+    cards = results.find_all("div", class_="card")
+    for c in cards:
+        title = c.find("h5", class_="card-title").text
+        date = c.find("h6", class_="card-subtitle").text
+        review = c.find("p", class_="card-text").text[8:].replace("\n", "\\n").replace('"', '""')
+        workload = c.find("p", class_="card-subtitle").text[10:].replace("\n", "\\n").replace('"', '""')
+        m = re.search('Agree: (\d+) \| Disagree: (\d+) \| Funny: (\d+)', c.text)
+        yield '"%s","%s","%s","%s","%s",%s,%s,%s\n' % (prof_name, title, date, review, workload, m.group(1),m.group(2),m.group(3))
+
+
+def getProfs(Url):
+    page = requests.get(Url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find("div", class_="col-sm-4 overflow-auto p-3 bg-light")
+    return results.find_all("a")
+
+
+if __name__ == "main":
+    with open("culpa.csv", "w") as f:
+        BaseURL = "https://culpaarchive.herokuapp.com"
+        profs = getProfs(BaseURL)
+        print("total:" + str(len(profs)))
+        f.write('"professor","class","date","review","workload","agree","disagree","funny"\n')
+        cnt = 1
+        for prof in profs:
+            result = getProfReview(BaseURL + prof['href'], prof.text)
+            for txt in result:
+                f.write(txt)
+            print("\r" + str(cnt) + " done", end="", flush=True)
+            cnt += 1
+
---
+++ utils/test_scraper.py
@@ -0,0 +1,22 @@
+import unittest
+import scraper
+
+class Test_Scraper(unittest.TestCase):
+    def setUp(self):
+        self.BaseURL = "https://culpaarchive.herokuapp.com"
+
+    def tearDown(self):
+        pass
+
+    def test_getProfs(self):
+        # test getProfs method
+        profs = scraper.getProfs(self.BaseURL)
+        self.assertEqual(len(profs), 1656)
+
+
+    def test_getProfReview(self):
+        # test getProfReview method
+        answer = ['"Agnieszka Legutko","Elementary Yiddish II","July 14, 2017","Agi is a lovely person with a fascinating story (she\'s a Polish Jewish Yiddishist). She\'s very approachable and definitely wants everyone in her class to succeed. I took this class in Spring 2017 so, unfortunately, I don\'t think I saw her at her best. She experienced some personal issues which we were all very sympathetic and flexible about but at a certain point it became clear that she should\'ve taken time off instead of continuing to work. She became forgetful and distracted (again, totally fair and human, just not conducive to teaching) to the point where she thought our Final started an hour later than it had been scheduled. As a person I understood but as a student her lack of organization/energy really frustrated and stressed me out as a student. The other thing I would say is that she is a better teacher for higher levels. There were several moments where some of the more adept students just answered other students\' questions for her because she didn\'t understand their question or explain the concept well. Again, I don\'t think Agi is a bad teacher, but I do think there are some things (whether temporary or permanent) that she could work on. ","There are homeworks from the book and packet, quizzes, a few journal entries, and a final. Overall pretty straightforward but it\'s important to stay on top of because it can be hard to catch up. You\'re pretty much expected to come to class to, y\'know, speak the language. The one other note I would say is that the Yiddish department takes off for Jewish holidays! (Whether you\'re Jewish or not this is a good thing to know).",1,0,0\n']
+        profs = scraper.getProfs(self.BaseURL)
+        self.assertEqual([e for e in scraper.getProfReview(self.BaseURL + profs[20]['href'], profs[20].text)], answer )
+
