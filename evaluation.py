fab0760c49ae8a0eee0653953d550a7e57bd86a3
Lee Pierre <pierre@LeePierre.com>
2021-12-10 21:07:59 -0500

    fix CI workflow

--- evaluation.py
+++ evaluation.py
@@ -6,6 +6,13 @@ import db
 import analysis
 import random
 import nltk
+import ssl
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
 nltk.download('punkt')
 nltk.download('wordnet')
 
